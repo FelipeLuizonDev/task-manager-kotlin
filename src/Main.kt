42e0ef469d24f998c2442d825785e90b772e3d91
FelipeLuizonDev <luizonfelipe@gmail.com>
2026-08-26 14:30:21 -0300

    feat: validate task title

--- src/Main.kt
+++ src/Main.kt
@@ -5,6 +5,12 @@ data class Task(
     val isCompleted: Boolean = false,
     val createdAt: String
 ) {
+    init {
+        require(title.isNotBlank()) {
+            "O título da tarefa não pode estar vazio."
+        }
+    }
+
     companion object {
         private var nextId = 1
 
